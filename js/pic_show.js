653727f6f24b43177601fed37743801626ddfe5f
Manfred Grauvogl <m.grauvogl@kabelmail.de>
2021-03-17 22:18:12 +0100

    Add files via upload

--- js/pic_show.js
+++ js/pic_show.js
@@ -17,10 +17,13 @@ var cap;
 var aus;
 var sec;
 var isvideo;
+var status = false;
+var bild_video;
 var bild_style; //the style of the small picture is saved
 //capt is the number of the caption in the array cap to be used 
 //if no caption is there for a video a number < 0 is used
 function show_img(fig, capt, bo){
+    bild_video = capt;
     isvideo=capt;
     bild = document.getElementById(fig);
     bild_style = bild.style;
@@ -32,12 +35,13 @@ function show_img(fig, capt, bo){
     bild.style.zIndex = "4";
     //center the image
     var centr = Math.round((sec.offsetWidth-bild.offsetWidth)/2);
+    console.log(sec.offsetWidth, bild.offsetWidth);
     bild.style.left = centr.toString()+ "px";
-    bild.style.top = "-1%";
+    bild.style.top = "-1%"; 
     //positioning of the figcaption
     var p = bild.offsetTop;
     var h = bild.height;
-    if (capt > -1) {
+    if (bild_video > -1) {
         var caparray = document.getElementsByClassName("cap");
         cap = caparray[capt];
         cap.style.display = "block";
@@ -47,9 +51,8 @@ function show_img(fig, capt, bo){
         cap.style.width = (bild.offsetWidth).toString()+"px";
         cap.style.zIndex = "4";
         //positioning of the close button
-    
         aus.style.display = "block";
-        var aus_breite = aus.offsetWidth;
+        var aus_breite = aus.offsetWidth*1.2;
         var aus_top = p;
         aus.style.top = aus_top.toString()+"px";
         var aus_left = bild.offsetLeft - aus_breite + bild.offsetWidth;
@@ -57,28 +60,28 @@ function show_img(fig, capt, bo){
         aus.style.zIndex = "5";
         aus.focus();
     }
+    satus = true;
     document.getElementById("clickon").style.display="none";
     if (capt < 0) bild.play();
 }
 
 //To keep everthing on place when the window is resized
 // capt < 0: no figure caption is there
-function resize_img(resimg, capt){
+function resize_img(resimg){
     //to keep pictures on place
     var w = document.getElementById(resimg);
     var h= stretch*w.offsetWidth;
     w.style.height = h.toString()+"px";
     //to keep the large picture in place
-    if (status === "true"){
+    if (status){
         //center the image
-        console.log(bild);
         var centr = Math.round((sec.offsetWidth-bild.offsetWidth)/2);
         bild.style.left = centr.toString()+ "px";
         bild.style.top = "-1%";
         //positioning of the figcaption
         var p = bild.offsetTop;
         var h = bild.height;
-        if (capt > -1){
+        if (bild_video > -1){
             cap.style.display = "block";
             cap.style.top = (p+h).toString()+"px";
             cap.style.left = centr.toString()+ "px";
@@ -86,7 +89,7 @@ function resize_img(resimg, capt){
         }
         //positioning of the close button
         aus.style.display = "block";
-        var aus_breite = aus.offsetWidth;
+        var aus_breite = aus.offsetWidth*1.2;
         var aus_top = p;
         aus.style.top = aus_top.toString()+"px";
         var aus_left = bild.offsetLeft - aus_breite + bild.offsetWidth;
@@ -100,5 +103,6 @@ function close_im(){
     if (isvideo > -1) cap.style.display = "none";
     bild.style = bild_style;
     document.getElementById("clickon").style.display="block";
+    satus = false;
 }
 
